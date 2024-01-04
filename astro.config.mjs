78caf0a034ec0a3c15865974e99292cfe9e1801b
Maaz <alimaazali6@gmail.com>
2024-01-04 22:44:14 +0530

    added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
+import vercel from '@astrojs/vercel/serverless';
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), mdx()],
-  output: "server"
+  output: "server",
+  adapter: vercel(),
 });
